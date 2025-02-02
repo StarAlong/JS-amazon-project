9fb4eea3570c70f8a151ec378c2a25fce52c0acd
jacklong233 <su1988112@gmail.com>
2025-02-02 17:40:08 +1100

    callbacks

--- data/products.js
+++ data/products.js
@@ -84,8 +84,32 @@ const object2 = {
 };
 */
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);   
+      }
+      return new Product(productDetails);
+  });
+  console.log('load products');
+
+  fun();
+});
 
 
+xhr.open('GET', 'https://supersimplebackend.dev/products');
+xhr.send();
+}
+
+
+
+
+/*
 export const products = [
   
  {
@@ -754,3 +778,4 @@ export const products = [
   return new Product(productDetails);
 });
 
+*/
